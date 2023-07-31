c51d54c3922e45a7d5878bb2dc363e804e5bdb63
James McGauran <jamesmcgauran11@gmail.com>
2023-07-31 10:18:00 +0100

    Tidy main

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 import "dotenv/config";
-import { Relay } from "nostr-tools";
 import { Editor, MarkdownView, Notice, Plugin, setIcon } from "obsidian";
 import { NostrModal } from "./src/NostrModal";
 import NostrService from "./src/nostr/NostrService";
@@ -7,28 +6,9 @@ import { NostrWriterSettingTab, loadSettings } from "./src/settings";
 
 export default class NostrWriterPlugin extends Plugin {
 	nostrService: NostrService;
-	relay: Relay;
 
 	async onload() {
 		this.nostrService = new NostrService("wss://relay.damus.io/");
-		// Initialize NostrService with the relay URL
-		// this.relay = relayInit("wss://relay.damus.io/");
-
-		// this.relay.on("connect", () => {
-		// 	console.log(`connected to ${this.relay.url}`);
-		// 	new Notice(`Connected to : ${this.relay.url}`);
-		// });
-
-		// this.relay.on("error", () => {
-		// 	console.error(`failed to connect to ${this.relay.url}`);
-		// 	new Notice(
-		// 		`Failed to connect to ${this.relay.url}. Check the console for more details.`
-		// 	);
-		// });
-
-		// this.relay.connect();
-
-		//await this.loadSettings();
 		await loadSettings(this);
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new NostrWriterSettingTab(this.app, this));
@@ -83,39 +63,6 @@ export default class NostrWriterPlugin extends Plugin {
 			},
 		});
 
-		// this.addCommand({
-		// 	id: "get-file-content",
-		// 	name: "Get File Content",
-		// 	callback: async () => {
-		// 		const file = this.app.workspace.getActiveFile();
-
-		// 		if (!file) {
-		// 			new Notice("No active file.");
-		// 			return;
-		// 		}
-
-		// 		try {
-		// 			const content = await this.app.vault.read(file);
-		// 			const success = await this.nostrService.publishNote(
-		// 				content
-		// 			);
-		// 			if (success) {
-		// 				new Notice("Content successfully published to Nostr.");
-		// 			} else {
-		// 				new Notice("Failed to publish content to Nostr.");
-		// 			}
-		// 		} catch (err) {
-		// 			console.error(
-		// 				`Failed to publish file content: ${file.path}`,
-		// 				err
-		// 			);
-		// 			new Notice(
-		// 				"An error occurred while trying to publish content."
-		// 			);
-		// 		}
-		// 	},
-		// });
-
 		this.addCommand({
 			id: "get-pub",
 			name: "Public Key",
@@ -148,117 +95,4 @@ export default class NostrWriterPlugin extends Plugin {
 	}
 
 	onunload() {}
-
-	// async loadSettings() {
-	// 	this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	// }
-
-	// async saveSettings() {
-	// 	await this.saveData(this.settings);
-	// }
-
-	// async publishNote() {
-	// 	console.log(`trying to publish note...`);
-
-	// 	// Assuming you want to publish the current active file
-	// 	const activeFile = this.app.workspace.getActiveFile();
-
-	// 	if (activeFile) {
-	// 		// Read the contents of the active file
-	// 		const fileContent = await this.app.vault.read(activeFile);
-	// 		console.log(fileContent);
-
-	// 		let tags: any = [["d", "lert25235"]];
-	// 		//   if(image) tags.push(["image", image]);
-	// 		//   if(summary) tags.push(["summary", summary]);
-	// 		//   if(publishedAt) tags.push(["published_at", publishedAt]);
-
-	// 		// Extract hashtags from content
-	// 		// const regex = /#\w+/g;
-	// 		// const matches = fileContent.match(regex) || [];
-	// 		// const hashtags = matches.map((match) => match.slice(1)); // Remove '#' from each hashtag
-
-	// 		// if (hashtags.length > 0) {
-	// 		// 	tags.push(["t", hashtags]);
-	// 		// 	console.log(hashtags);
-	// 		// }
-
-	// 		const regex = /#\w+/g;
-	// 		const matches = fileContent.match(regex) || [];
-	// 		const hashtags = matches.map((match) => match.slice(1)); // Remove '#' from each hashtag
-
-	// 		for (const hashtag of hashtags) {
-	// 			tags.push(["t", hashtag]);
-	// 			console.log(hashtag);
-	// 		}
-
-	// 		const noteTitle = activeFile.basename;
-	// 		console.log(noteTitle);
-	// 		tags.push(["title", noteTitle]);
-	// 		// TODO: need to check for NIP-19 keys liek these and convert if needed
-	// 		let privateKeyEnv = this.getEnvVar("NOSTR_PRIVATE_KEY");
-	// 		console.log(`privateKeyEnv: ${privateKeyEnv}`)
-	// 		let privateKey =
-	// 		"xxxx";
-	// 		let decodedPrivateKey = nip19.decode(privateKey)
-	// 		console.log(decodedPrivateKey.data)
-	// 		let publicKey =
-	// 			"xxxxxxx";
-	// 		let decodedPublicKey = nip19.decode(publicKey)
-
-	// 		console.log(decodedPublicKey.data)
-
-	// 		let eventTemplate: EventTemplate<Kind.Text> = {
-	// 			//kind: 30023,
-	// 			kind: 1,
-	// 			created_at: Math.floor(Date.now() / 1000),
-	// 			tags: tags,
-	// 			content: fileContent,
-	// 		};
-	// 		console.log(eventTemplate);
-
-	// 		let event: UnsignedEvent<Kind.Text> = {
-	// 			...eventTemplate,
-	// 			pubkey: decodedPublicKey.data as string,
-	// 		};
-
-	// 		console.log(`Event: ${event.content} & ${event.pubkey} & ${event.created_at} & ${event.kind} & ${event.tags} `);
-	// 		let eventHash = getEventHash(event);
-	// 		try {
-	// 			let finalEvent: Event<Kind.Text> = {
-	// 				...event,
-	// 				id: eventHash,
-	// 				sig: getSignature(event, decodedPrivateKey.data as string),
-	// 			};
-	// 			// Publish the file content
-	// 			//await this.nostrService.publishNote(fileContent);
-	// 			//let privateKey = this.getEnvVar("NOSTR_PRIVATE_KEY");
-	// 			// event.id = getEventHash(event);
-	// 			// event.sig = getSignature(event, privateKey);
-	// 			// serializeEvent(event);
-	// 			console.log(`Final Event: ${finalEvent.content} `);
-	// 			let pub = this.relay.publish(finalEvent);
-
-	// 			pub.on("ok", () => {
-	// 				console.log(`Event published successfully`);
-	// 				new Notice("Event published successfully ");
-	// 				new Notice(`Event published successfully ${event.content}`);
-	// 				// TODO save event data to logs or something
-	// 			});
-
-	// 			pub.on("failed", (reason: any) => {
-	// 				console.log(`Failed to publish event: ${reason}`);
-	// 				new Notice(`Failed to publish event: ${reason}`);
-	// 			});
-
-	// 			// Show a success notification
-	// 			new Notice("Successfully published note to Nostr.");
-	// 		} catch (error) {
-	// 			// Show an error notification
-	// 			new Notice(
-	// 				`Failed to publish note to Nostr..: ${error.message}`
-	// 			);
-	// 		}
-	// 	}
-	// }
 }
