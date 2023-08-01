4c36c536d9425bcca0d105693fbd40c1797a5068
James McGauran <jamesmcgauran11@gmail.com>
2023-08-01 18:51:14 +0100

    Remove react deps and files

--- src/ReactView.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import { useApp } from "./hooks";
-
-export const ReactView = () => {
-  const { vault } = useApp();
-  return <h4>{vault.getName()}</h4>;
-};
--- src/hooks.ts
+++
@@ -1,7 +0,0 @@
-import { App } from "obsidian";
-import { AppContext } from "./context";
-import * as React from "react";
-
-export const useApp = (): App | undefined => {
-  return React.useContext(AppContext);
-};
